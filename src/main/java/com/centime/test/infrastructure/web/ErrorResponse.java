51631de2cea7bffa15cec346477035dea41706b3
pulgamprem <pulgamprem@gmail.com>
2020-07-17 14:34:23 +0530

    init

---
+++ src/main/java/com/centime/test/infrastructure/web/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.centime.test.infrastructure.web;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+/**
+ * Object representing an Error to be sent to the API caller incase of an Error.
+ *
+ * @author Prem
+ */
+@Data
+@Builder
+class ErrorResponse {
+
+    private final HttpStatus status;
+    private final List<String> errors;
+
+    private String description;
+}
---
+++ src/main/java/com/centime/test/model/Person.java
@@ -0,0 +1,15 @@
+package com.centime.test.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private String name;
+    private String surName;
+}
